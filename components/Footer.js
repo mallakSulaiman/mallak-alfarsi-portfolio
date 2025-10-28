1dcbfe2160ff587294c24ff31afeb5c6c64ffdfa
mallak Sulaiman <mallak@mass4.om>
2025-10-29 01:45:51 +0400

    bj

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container-custom text-center">
-        <p>&copy; 2024 Mallak Sulaiman Nasser Alfarsi. All rights reserved.</p>
+        <p>&copy; 2024 Mallak Sulaiman Alfarsi. All rights reserved.</p>
         <p className="mt-2 text-gray-400">
           Software Engineer | Web Developer
         </p>
